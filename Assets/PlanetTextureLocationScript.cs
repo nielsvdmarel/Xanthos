30dd55d6a841d592692a0b62fb41164df42ae1bb
nielsvdmarel <nielsvdmarel@gmail.com>
2017-12-10 23:44:58 +0100

    Cords system up and down working! :+1:

--- Assets/PlanetTextureLocationScript.cs
+++ Assets/PlanetTextureLocationScript.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlanetTextureLocationScript : MonoBehaviour {
     public Vector2 Planetlocation;
@@ -11,48 +12,62 @@ public class PlanetTextureLocationScript : MonoBehaviour {
     private int[] TextureColorCords = new int[262144];
     private Color[] texCords = new Color[26144];
     [SerializeField]
-    private Color kanker;
+    private List<Vector2> CurrentActivePixels;
+    public int largenum;
+    public Color MainColor;
 
+    //cords
+    public int x;
+    public int y;
     [SerializeField]
-    private int currennumb;
+    private int CheckSpaceHalf;
+    [SerializeField]
+    private int FullScanPixels;
 
     public Color testColor;
 
     void Start ()
     {
         SetTextureColorCords();
-	}
+        CurrentActivePixels = new List<Vector2>();
+    }
 	
 	void Update ()
     {
         FixCords();
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             Planetlocation.y += walkSpeed;
-            currennumb++;
-            
+            GetTexturePointUp();
+            GetTexturePointDown();
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             GetTexturePoint();
-
+            GetTexturePointUp();
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             Planetlocation.y -= walkSpeed;
+            GetTexturePointUp();
+            GetTexturePointDown();
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             Planetlocation.x += walkSpeed;
+            GetTexturePointUp();
+            GetTexturePointDown();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             Planetlocation.x -= walkSpeed;
+            GetTexturePointUp();
+            GetTexturePointDown();
         }
     }
 
@@ -89,13 +104,66 @@ public class PlanetTextureLocationScript : MonoBehaviour {
         //testColor = texCords[250];
         //x plus y keer breedte 
         //currennumb = 118272;
-        testColor = texCords[currennumb];
-        for (int i = 0; i < texCords.Length; i++)
+        //0 = 500
+        //500 - 40 = 460;
+       
+        largenum =  x  + ((PlanetMap.height - y) * PlanetMap.width);
+        testColor = texCords[largenum];
+    }
+
+    void GetTexturePointUp()
+    {
+        int uppointx = Mathf.RoundToInt(Planetlocation.x);
+        int uppointy = Mathf.RoundToInt(Planetlocation.y);
+        uppointx -= CheckSpaceHalf;
+        uppointy += CheckSpaceHalf;
+
+        int checkpointUp = uppointx + ((PlanetMap.height - uppointy) * PlanetMap.width);
+        for (int i = 0; i < FullScanPixels; i++)
         {
-            if (texCords[i] == kanker)
+            testColor = texCords[checkpointUp + i];
+            if(testColor != MainColor)
             {
-                Debug.Log(i);
+                Vector2 CurrentAdd = new Vector2(uppointx += i, uppointy);
+                if (!CurrentActivePixels.Contains(CurrentAdd))
+                {
+                    CurrentActivePixels.Add(CurrentAdd);
+                }
+
+                else if(CurrentActivePixels.Contains(CurrentAdd))
+                {
+                    CurrentActivePixels.Remove(CurrentAdd);
+                }
             }
         }
+        
+    }
+
+    void GetTexturePointDown()
+    {
+        int Downpointx = Mathf.RoundToInt(Planetlocation.x);
+        int Downpointy = Mathf.RoundToInt(Planetlocation.y);
+        Downpointx -= CheckSpaceHalf;
+        Downpointy -= CheckSpaceHalf;
+
+        int checkpointDown = Downpointx + ((PlanetMap.height - Downpointy) * PlanetMap.width);
+        for (int i = 0; i < FullScanPixels; i++)
+        {
+            testColor = texCords[checkpointDown + i];
+            if (testColor != MainColor)
+            {
+                Vector2 CurrentAdd = new Vector2(Downpointx += i, Downpointy);
+                if (!CurrentActivePixels.Contains(CurrentAdd))
+                {
+                    CurrentActivePixels.Add(CurrentAdd);
+                }
+
+                else if (CurrentActivePixels.Contains(CurrentAdd))
+                {
+                    CurrentActivePixels.Remove(CurrentAdd);
+                }
+            }
+        }
+
     }
 }
