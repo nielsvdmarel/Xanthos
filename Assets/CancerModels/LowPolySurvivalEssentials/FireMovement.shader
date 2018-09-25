ba4cac32f07f777016af8e156c624589b0aae5fe
nielsvdmarel <nielsvdmarel@gmail.com>
2018-09-26 00:23:24 +0200

    Splash screen

---
+++ Assets/CancerModels/LowPolySurvivalEssentials/FireMovement.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/FireMovement" {
+	Properties{
+		_MainTex("Texture", 2D) = "white" {}
+	_TimeScale("Time Scaling", Range(0,10)) = 1.0
+		_XAmount("X Wiggle", Range(0,5)) = 1.0
+		_YAmount("Y Wiggle", Range(0,5)) = 1.0
+	}
+		SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		Cull Off
+		CGPROGRAM
+#pragma surface surf Lambert vertex:vert
+
+
+		struct Input {
+		float2 uv_MainTex;
+	};
+	float _XAmount;
+	float _YAmount;
+	float _TimeScale;
+	void vert(inout appdata_full v)
+	{
+		float time = _TimeScale * _Time.y;
+		float iny = v.vertex.y * _YAmount + time;
+		float wiggleX = sin(iny) * _XAmount;
+		float wiggleY = cos(iny) * _XAmount;
+		v.normal.y = v.normal.y + wiggleY;
+		normalize(v.normal);
+		v.vertex.x = v.vertex.x + wiggleX;
+
+	}
+
+	sampler2D _MainTex;
+
+	void surf(Input IN, inout SurfaceOutput o) {
+
+		o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb;
+
+	}
+	ENDCG
+	}
+		Fallback "Diffuse"
+}
---
+++ Assets/Scripts/Camera/SplashScreenCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class SplashScreenCamera : MonoBehaviour {
+    [SerializeField]
+    private PostProcessingProfile cc;
+    [SerializeField]
+    private Animator AnimController;
+    private DepthOfFieldModel UpDof;
+    private DepthOfFieldModel DownDof;
+    private DepthOfFieldModel.Settings settings;
+
+    void Start ()
+    {
+        AnimController = this.gameObject.GetComponent<Animator>();
+        cc = this.GetComponent<PostProcessingBehaviour>().profile;
+        settings = cc.depthOfField.settings;
+	}
+	
+	void Update ()
+    {
+        if (AnimController.GetCurrentAnimatorStateInfo(0).normalizedTime > .65)
+        {
+            settings.aperture = (float)Mathf.Lerp(settings.aperture, .7f, Time.deltaTime / 2f);
+            cc.depthOfField.settings = settings;
+        }
+
+        if (AnimController.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            //Debug.Log("sds");
+        }
+    }
+}
